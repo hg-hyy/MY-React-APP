504b2b04ff9de143309729db78bccd851da2af26
hyy <1021509854@qq.com>
2022-08-03 21:31:01 +0800

    up

--- src/actions/types.js
+++
@@ -1,9 +0,0 @@
-export const ADD_TO_CART = "ADD_TO_CART";
-export const UPDATE_CART = "UPDATE_CART";
-export const DELETE_FROM_CART = "DELETE_FROM_CART";
-export const SELETE_FROM_CART = "SELETE_FROM_CART";
-export const DELETE_FROM_CART_ID = "DELETE_FROM_CART_ID";
-export const GET_ERRORS = "GET_ERRORS";
-export const SET_LOGIN_IN = "SET_LOGIN_IN";
-export const SET_LOGIN_OUT = "SET_LOGIN_OUT";
-export const SET_SIGN_UP = "SET_SIGN_UP";

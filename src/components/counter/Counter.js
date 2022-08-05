e517f81485d7b409997e25afc67d0798157dd9dd
hyy <littleshenyun@outlook.com>
2022-08-05 15:46:27 +0800

    tinymce

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,33 +1,40 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment, login } from "../../reducers/counterSlice";
-
+import Tinymce from "./Tinymce";
 export default function Counter() {
   const count = useSelector((state) => state.counterReducer.value);
   const isauth = useSelector((state) => state.counterReducer.isauth);
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <div>
+    <div className="m-3">
+      <div className="d-flex flex-row m-3 justify-content-start  align-items-center align-self-center align-content-center">
+        <span className="h3">{count}</span>
         <button
+          className="btn btn-primary"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Increment
         </button>
-        <button aria-label="Increment value" onClick={() => dispatch(login())}>
-          login
-        </button>
-        <span>{count}</span>
-        {isauth ? <span>true</span> : <div>false</div>}
         <button
+          className="btn btn-info"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           Decrement
         </button>
+        <button
+          className="btn btn-success"
+          aria-label="Increment value"
+          onClick={() => dispatch(login())}
+        >
+          login
+        </button>
+        {isauth ? <span className="h3">true</span> : <div>false</div>}
       </div>
+      <Tinymce />
     </div>
   );
 }
